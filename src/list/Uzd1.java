954c5ff86031e14adf872c6ef13de8bf91d72c0a
qwe <h@gmail.com>
2025-12-03 08:04:56 +0200

    щperma

--- src/list/Uzd1.java
+++ src/list/Uzd1.java
@@ -1,5 +1,6 @@
 package list;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -28,7 +29,9 @@ public class Uzd1 {
 	public static void main(String[]args)  {
 
 		String izvele, koPiev, koAtr, koNon, arKoAizst;
-		int kurPiev, kurNonm, kuruMain;
+		int kurPiev, kurNon, kuruMain;
+		Object[] atbilde = {"Jā", "Nē"};
+
 	LinkedList<String> saraksts = new LinkedList<>();
 	
 		do {
@@ -84,10 +87,78 @@ public class Uzd1 {
 					"Produkts atrasts"+saraksts.indexOf(koAtr)+". pozīcijā~" : "Produkts netika atrasts~", "Informācija", JOptionPane.INFORMATION_MESSAGE);
 			
 		break;
+		case "5":
+			do {
+				koPiev = JOptionPane.showInputDialog("Kādu produktu pievienot?");					
+				kurPiev = Integer.parseInt(JOptionPane.showInputDialog("Kurā pozīcijā pievienot?"));
+				
+				if( kurPiev < 1 || kurPiev >= saraksts.size()) JOptionPane.showMessageDialog(null, "Nedrīkst pievienot produktu šājā pozīcijā~", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+			}while((jauEksiste(saraksts, koPiev) == true) ||
+					!koPiev.matches("[\\p{L} ]+$") && kurPiev < 1 || kurPiev > saraksts.size());
+			saraksts.add(kurPiev-1, koPiev);
+		break;
+		case "6":
+			do {
+				koNon = JOptionPane.showInputDialog("Kuru produktu noņemt?");					
+				
+			}while(!koNon.matches("[\\p{L} ]+$") || !jauEksiste(saraksts, koNon)) ;
+			if(saraksts.remove(koNon.toLowerCase())) JOptionPane.showMessageDialog(null, "Produkts noņemts", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			else JOptionPane.showMessageDialog(null, "Produkts nav noņemts", "Paziņojums", JOptionPane.WARNING_MESSAGE);
+		break;
+		case "7":
+			do {
+			kurNon = Integer.parseInt(JOptionPane.showInputDialog("Kuras pozīcijas elementu noņemt?"));
+			}while(kurNon < 1 || kurNon > saraksts.size());
+			saraksts.remove(kurNon-1);
+		break;
+		case "8":
+			saraksts.removeFirst();
+		break;
+		case "9":
+			saraksts.removeLast();
+		break;
+		case "10":
+			
+		break;
+		case "11":
+			if(saraksts.size()>0) {
+				String atb = (String) JOptionPane.showInputDialog(null, "Kārtot produktus pēc alfabēta augoši?", "Izvēle", JOptionPane.QUESTION_MESSAGE,
+						null, atbilde, atbilde[0]);
+				if(atb != null) {
+					if(atb.equals("Jā")) {
+						saraksts.sort(null);
+						JOptionPane.showMessageDialog(null, "Produkti sakārtoti augoši", "Kārtošana", JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						saraksts.sort(Collections.reverseOrder());
+						JOptionPane.showMessageDialog(null, "Produkti sakārtoti dilstoši", "Kārtošana", JOptionPane.INFORMATION_MESSAGE);
+
+					}
+				}
+				
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "Sarakstā nav neviens produkts!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				break;
+			}
+			break;
+		case "12":
+			if(saraksts.size()>0) {
+
+			String ties = (String) JOptionPane.showInputDialog(null, "Vai tieši gribāt dzēst visu sarakstu?~", "Izvēle", JOptionPane.QUESTION_MESSAGE,
+					null, atbilde, atbilde[0]);
+			if(ties == null || ties == "Nē") break; 
+			else{ saraksts.clear();
+			JOptionPane.showMessageDialog(null, "Viss saraksts attīrīts!", "Ziņojums", JOptionPane.INFORMATION_MESSAGE); }
+			}else {
+				JOptionPane.showMessageDialog(null, "Sarakstā nav neviens produkts!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				break;
+			}
+		break;
+		case "0":
+			JOptionPane.showMessageDialog(null, "Programma apturēta!", "Paziņojums", JOptionPane.PLAIN_MESSAGE);
+		break;
+		default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
 		}
-	
-	
-	
 	}
 
 }
